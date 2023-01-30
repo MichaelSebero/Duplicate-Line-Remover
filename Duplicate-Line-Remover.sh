b4c31cdf1282a10623c1e226887e04526d9e50a4
Michael Sebero <106393611+MichaelSebero@users.noreply.github.com>
2023-01-30 09:33:38 -0500

    Update Duplicate-Line-Remover.sh

--- Duplicate-Line-Remover.sh
+++ Duplicate-Line-Remover.sh
@@ -3,5 +3,5 @@
 # License: GNU GPLv3
 
 read -p "Location of file: " locationoffile
-sudo touch /home/output
-sudo sort -u $locationoffile --output=/home/output
+sudo touch /home/$USER/Desktop/output
+sudo sort -u $locationoffile --output=/home/$USER/Desktop/output
